7cd9cde9aa5e0445eb4659734cb55ed7e2704835
catd87 <carlos.torresduran@gmail.com>
2022-07-01 20:12:28 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,4 +2,5 @@ module "network" {
   source  = "Azure/network/azurerm"
   version = "3.5.0"
   subnet_names  = [ "subnet1" ]
+  resource_group_name       = azurerm_resource_group.myresourcegroup.name
 }
