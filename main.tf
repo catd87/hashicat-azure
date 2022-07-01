bdfdd69ba8ac05d3d1dd38f35b3cd2d22a5c7a02
catd87 <carlos.torresduran@gmail.com>
2022-07-01 18:15:50 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -136,8 +136,10 @@ resource "azurerm_virtual_machine" "catapp" {
   }
 
   tags = {
-    Name = "Department"
+    "Department"
+    Owner = "DevOps"
   }
+
 
   # Added to allow destroy to work correctly.
   depends_on = [azurerm_network_interface_security_group_association.catapp-nic-sg-ass]
